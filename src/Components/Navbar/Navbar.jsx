04b804fd4a08491c040b4dc0468ed01f9fbb4ffb
Raj-Mayank2 <mayank170403@gmail.com>
2024-12-17 07:45:58 +0530

    some changes

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -14,19 +14,7 @@ const Navbar = () => {
         <li><Link to="/about">About</Link></li>
         <li><Link to="/gallery">Gallery</Link></li>
         <li><Link to="/teams">Teams</Link></li>
-        <li>
-        {isAuthenticated && <p>{user.name}</p>}
-        </li>
-        {
-          isAuthenticated ? <li>
-            <button className='btn1' onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}>
-              Log Out
-            </button>
-          </li>
-            : <li>
-              <button className='btn1' onClick={() => loginWithRedirect()}>Log In</button>
-            </li>
-        }
+        
 
 
       </ul>
--- src/pages/Teams/Teams.jsx
+++ src/pages/Teams/Teams.jsx
@@ -3,6 +3,16 @@ import './Teams.css';
 import mayank from '../../assets/members/mayank.jpg'
 import dhiraj from '../../assets/members/dhiraj.jpg'
 import prince from '../../assets/members/prince.jpg'
+
+import aman from '../../assets/alumni/aman.jpg'
+import aritra from '../../assets/alumni/aritra.jpg'
+import shuvajit from '../../assets/alumni/shuvajit.jpg'
+
+import sudhansu from '../../assets/alumni/sudhansu.jpg'
+import rupam from '../../assets/alumni/rupam.png'
+import ebrahim from '../../assets/alumni/ebrahim.png'
+import diwakar from '../../assets/alumni/diwakar.jpg'
+
 import anurag from '../../assets/members/anurag.jpg'
 import wasim from '../../assets/members/wasim.jpg'
 import raj from '../../assets/members/raj.jpg'
@@ -38,7 +48,13 @@ const teamData = {
       position: "Secretary",
       skills: "Web Developer, App Developer",
       image: piyush
-    }
+    },
+    {
+      name: "Bhaskar Sarkar",
+      position: "Current Supervisor",
+      skills: "Web Development",
+      image: profile2
+    },
   ],
   members: [
     {
@@ -49,7 +65,7 @@ const teamData = {
     },
     {
       name: "Dhiraj Singh",
-      position: "Current Supervisor",
+      position: "PR and Sponsor Management",
       skills: "Web Development",
       image: dhiraj
     },
@@ -71,12 +87,7 @@ const teamData = {
       skills: "Web Development",
       image: sumit
     },
-    {
-      name: "Bhaskar Haldar",
-      position: "External Affairs Manager",
-      skills: "Web Development",
-      image: profile2
-    },
+    
     {
       name: "Prince Kumar",
       position: "App Development Lead",
@@ -96,6 +107,80 @@ const teamData = {
       image: antika
     }
   ],
+  former:[
+    {
+      name: "Md. Ilham Ansari",
+      position: "Former Lead",
+      skills: "Web Development",
+      image: ilham
+    },
+    {
+      name: "Nirvik Ghosh",
+      position: "Former Secretary & Technical Lead",
+      skills: "Web Development",
+      image: nirvik
+    },
+    {
+      name: "Astha Samaddar",
+      position: "Former Supervisor",
+      skills: "Web Development",
+      image: astha
+    },
+    
+    
+    
+    {
+      name: "Tilak Ghosh",
+      position: "Former Team Member",
+      skills: "Web Development",
+      image: tilak
+    },
+    {
+      name: "Dilip Sarkar",
+      position: "Former Team Member",
+      skills:"Web Development",
+      image: dilip
+    },
+    
+    {
+      name: "Ritabrata Chakraborty",
+      position: "Former Team Member",
+      skills: "Web Development",
+      image: ritabrata
+    },
+    
+    
+    {
+      name: "Diwakar Pandit",
+      position: "Former Team Member",
+      skills: "Web Development",
+      image: diwakar
+    },
+    {
+      name: "Avik Mukherjee",
+      position: "Former Team Member",
+      skills:"Web Development",
+      image: avik
+    },
+    {
+      name: "Indraneel Sarkar",
+      position: "Former Team Member",
+      skills: "Web Development",
+      image: indraneel
+    },
+    {
+      name: "Joyjit Das",
+      position: "Former Team Member",
+      skills:"Web Development",
+      image: joyjit
+    },
+    {
+      name: "Priti Gupta",
+      position: "Former Team Member",
+      skills: "Web Development",
+      image: priti
+    }
+  ],
   alumni: [
     {
       name: "Arghadeep Mallick",
@@ -109,75 +194,43 @@ const teamData = {
       skills: "Web Development",
       image: suman
     },
-    
     {
-        name: "Md. Ilham Ansari",
-        position: "Former Lead",
-        skills: "Web Development",
-        image: ilham
-      },
-      {
-        name: "Nirvik Ghosh",
-        position: "Former Secretary & Technical Lead",
-        skills: "Web Development",
-        image: nirvik
-      },
-      {
-        name: "Astha Samaddar",
-        position: "Former Supervisor",
-        skills: "Web Development",
-        image: astha
-      },
-      {
-        name: "Ritam Mishra",
-        position: "Former Team Member",
-        skills: "Web Development",
-        image: ritam
-      },
-      {
-        name: "Indraneel Sarkar",
-        position: "Former Team Member",
-        skills: "Web Development",
-        image: indraneel
-      },
-      {
-        name: "Joyjit Das",
-        position: "Former Team Member",
-        skills:"Web Development",
-        image: joyjit
-      },
-      
-      {
-        name: "Tilak Ghosh",
-        position: "Former Team Member",
-        skills: "Web Development",
-        image: tilak
-      },
-      {
-        name: "Dilip Sarkar",
-        position: "Former Team Member",
-        skills:"Web Development",
-        image: dilip
-      },
-      
-      {
-        name: "Ritabrata Chakraborty",
-        position: "Former Team Member",
-        skills: "Web Development",
-        image: ritabrata
-      },
-      {
-        name: "Avik Mukherjee",
-        position: "Former Team Member",
-        skills:"Web Development",
-        image: avik
-      },
-      {
-        name: "Priti Gupta",
-        position: "Former Team Member",
-        skills: "Web Development",
-        image: priti
-      }
+      name: "Rupam Dey",
+      position: "Founder Marketing Head",
+      skills: "Web Development",
+      image: rupam
+    },
+    {
+      name: "Shuvajit Maity",
+      position: "Founder Team Member",
+      skills: "Web Development",
+      image: shuvajit
+    },
+    {
+      name: "Aritra Dian",
+      position: "Founder Team Member",
+      skills: "Web Development",
+      image:aritra
+    },
+    {
+      name: "Md. Ebrahim",
+      position: "Founder Team Member",
+      skills: "Web Development",
+      image: ebrahim
+    },
+    {
+      name: "Aman Sharma",
+      position: "Founder Team Member",
+      skills: "Web Development",
+      image: aman
+    },
+    {
+      name: "Sudhanshu Kumar",
+      position: "Founder Team Member",
+      skills: "Web Development",
+      image: sudhansu
+    },
+
   ]
 }
 
@@ -204,6 +257,7 @@ const Teams = () => {
       <TeamSection title="Our Leads" members={teamData.leads} />
       <TeamSection title="Our Team Members" members={teamData.members} />
       <TeamSection title="Our Alumni" members={teamData.alumni} />
+      <TeamSection title="Our Former Members" members={teamData.former} />
     </div>
   )
 }
